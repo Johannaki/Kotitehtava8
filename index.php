7738363b337d1be3c428a33b31512f1d142a2d4f
Johanna Kinnari <johanna.kinnari@metropolia.fi>
2018-04-26 11:04:10 +0300

    Aloitus

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+   <head>
+     <meta charset="UTF-8">
+     <title></title>
+     <style>
+     </style>
+   </head>
+   <body>
+
+
+     <form action="kuvat.php" enctype="multipart/form-data" method="POST">
+       <input type="file" name="kuva" />
+       <input type="submit" name="submit" value="Lataa" />
+     </form>
+   </body>
+</html>
