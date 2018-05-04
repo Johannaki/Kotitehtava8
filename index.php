fa501747ce2511b16abeac3813d2217675467038
Johanna Kinnari <johanna.kinnari@metropolia.fi>
2018-05-04 11:00:33 +0300

    pe klo11

--- index.php
+++ index.php
@@ -7,11 +7,32 @@
      </style>
    </head>
    <body>
+     <nav>
+       <ul>
+         <li><a href="index.php">Etusivu</a></li>
+         <li><a href="kuvat.php">Ladatut kuvat</a></li>
+       </ul>
+     </nav>
 
+<?php
+  if(isset($_POST['submit'])) {
+    $tmp_file = $_FILES['kuva']['tmp_name'];
+    $target_file = basename($_FILES['kuva']['name']);
+    $upload_dir = "uploads";
 
-     <form action="kuvat.php" enctype="multipart/form-data" method="POST">
+    if(move_uploaded_file($tmp_file, $upload_dir."/".$target_file)){
+      echo "Tiedosto ladattu onnistuneesti.";
+    } else {
+      echo $_FILES['kuva']['error'];
+    }
+  }
+?>
+     <form action="index.php" enctype="multipart/form-data" method="POST">
        <input type="file" name="kuva" />
+       Nimi: <input type="text" name="nimetty"><br>
        <input type="submit" name="submit" value="Lataa" />
      </form>
+
+
    </body>
 </html>
